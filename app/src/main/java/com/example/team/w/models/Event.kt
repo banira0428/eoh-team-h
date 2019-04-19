7a210cb76be9748d6bbf59fa67cad438e4d74c34
山田敬汰 <banira0428@gmail.com>
2019-04-19 21:47:39 +0900

    data class 作成

---
+++ app/src/main/java/com/example/team/w/models/Event.kt
@@ -0,0 +1,5 @@
+package com.example.team.w.models
+
+import java.util.*
+
+data class Event(val id:String = "",val name: String = "",val description: String = "",val date: Date = Date(),val imagePath: String = "")
---
+++ app/src/main/java/com/example/team/w/models/Project.kt
@@ -0,0 +1,5 @@
+package com.example.team.w.models
+
+import java.util.*
+
+data class Project(val id: String = "",val name: String = "",val date: Date = Date(),val imagePath: String = "")
