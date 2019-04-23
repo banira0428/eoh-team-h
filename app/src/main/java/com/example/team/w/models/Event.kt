6bef100e4c4cc495d7b9308f713daa3edebd6b3b
SoutaHagiwara <0104.seibu6000@gmail.com>
2019-04-23 10:30:11 +0900

    fix

--- app/src/main/java/com/example/team/w/models/Event.kt
+++ app/src/main/java/com/example/team/w/models/Event.kt
@@ -2,4 +2,11 @@ package com.example.team.w.models
 
 import java.util.*
 
-data class Event(val id:String = "",var name: String = "",var desc: String = "",val image_url: String = "",var wareki: Int = 0,val sent_at: Long = Date().time)
+data class Event(
+        val id:String = "",
+        var name: String = "",
+        var desc: String = "",
+        val image_url: String = "",
+        var wareki: Int = 0,
+        val sent_at: Long = Date().time
+)
